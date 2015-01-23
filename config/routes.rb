1dbd56dc2349dbc220a35f16ded6d1e681be0f39
Abena Ofori <abenaboama@gmail.com>
2015-01-22 21:39:16 -0500

    First commit and README update

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,4 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-
-  get 'welcome/about'
-
-<<<<<<< HEAD
-=======
-     root to: 'welcome#index'
-
->>>>>>> modify-html
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
